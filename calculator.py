02b2a8a5d5723660bd661ebf811f2934bcadceed
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2024-07-31 01:16:54 +0530

    This is my first excersise to create a calculator using python

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#Create a program which performs the arithematic calculations of 2 numbers:
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("The sum of two numbers is: ", a+b)
+print("The difference of two numbers is: ", a-b)
+print("The product of two numbers is: ", a*b)
+print("The quotinet is: ", a/b)
+print("The modular of the number is: ", a%b)
+print("The answer is: ", a//b)
+print("The square of the number is: ", a**b)
+
+
